a26ea549d0dfeab6c09d43050783b0a87b4feaae
Babybab <316067260@qq.com>
2019-03-07 13:53:08 +0800

    提交文件

---
+++ src/main/java/Task.java
@@ -0,0 +1,8 @@
+import android.content.Context;
+import android.widget.Toast;
+
+public class Task {
+    public static void test(Context context){
+        Toast.makeText(context, "试试能用不", Toast.LENGTH_SHORT).show();
+    }
+}
